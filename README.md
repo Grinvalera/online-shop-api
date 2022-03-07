403e98130ecdfe8e81ab9451461b54f04ff18f4a
grinvalera <valera.grinevich@gmail.com>
2022-03-07 20:38:54 +0200

    Creating authorization and registration

---
+++ README.md
@@ -0,0 +1,8 @@
+##How to setup project
+
+1. git clone https://github.com/Grinvalera/online-shop-api.git
+2. composer install
+3. cp .env.example .env
+4. php artisan key:generate
+5. php artisan migrate
+6. php artisan serve
---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+    }
+
+    /**
+     * @param Request $request
+     * @return mixed
+     */
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+           'name' => 'required|max:256',
+           'email' => 'required|unique:users,email|email|max:256|min:6',
+           'password' => 'required|max:256'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $data = $validator->validate();
+
+        return User::create([
+           'name' => $data['name'],
+           'email' => $data['email'],
+           'password' => Hash::make($data['password'])
+        ]);
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required|string|min:6',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        if (!$token = auth('api')->attempt($validator->validated())) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    /**
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout()
+    {
+        auth('api')->logout();
+
+        return response()->json(['message' => 'User successfully logged out.']);
+    }
+
+    /**
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function refresh()
+    {
+        return $this->respondWithToken(auth()->refresh());
+    }
+
+    /**
+     * @param $token
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth('api')->factory()->getTTL() * 60
+        ]);
+    }
+}
